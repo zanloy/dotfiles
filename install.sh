6fddff6ce4f2d476ddb6e15ff0c6b0cf5560baf2
Zan Loy <zan.loy@gmail.com>
2019-06-16 13:29:08 -0400

    fixed the install script

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 if [ ! -d "$HOME/.dotfiles" ]; then
   echo "Installing .dotfiles..."
-  git clone --depth=1 https://github.com/zanloy/dotfiles.git "$HOME/.dotfiles"
+  git clone --depth=1 --recurse-submodules -j8 https://github.com/zanloy/dotfiles.git "$HOME/.dotfiles"
+  # TODO: Figure out why the submodules aren't getting pulled in the above command.
   cd "$HOME/.dotfiles"
+  git submodule update --init --recursive -j8
   rake install
 else
   echo ".dotfiles is already installed"
