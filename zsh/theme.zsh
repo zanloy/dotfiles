2b86c324b01a409dcaa90c5de1330ad3c46100d9
Zan Loy <zan.loy@gmail.com>
2020-02-19 13:20:38 -0500

    updating to use powerlevel10k for prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,4 +1,4 @@
 prompt powerlevel10k
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ ! -f ~/.dotfiles/zsh/p10k.config ]] || source ~/.dotfiles/zsh/p10k.config
