20586cab1ad4f31a6171b28db5b60fb44daf4ef2
Zan Loy <zan.loy@gmail.com>
2020-10-28 14:59:12 -0400

    added to kubectl functions

--- zsh/kubectl.zsh
+++ zsh/kubectl.zsh
@@ -1,13 +1,3 @@
-use-mk() {
-  if [[ -x /snap/bin/microk8s.kubectl ]]; then
-    source <(/snap/bin/microk8s.kubectl completion zsh)
-    complete -F __start_kubectl k
-    alias kns='mkubectl config set-context --current --namespace'
-  else
-    echo "ERROR: /snap/bin/microk8s.kubectl was not found."
-  fi
-}
-
 if [[ -x "$(command -v kubectl)" ]]; then
   source <(kubectl completion zsh)
   alias k='kubectl'
@@ -19,3 +9,37 @@ if [[ -x "$(command -v kubectl)" ]]; then
   alias -- find-pod="kubectl get pods --all-namespaces | grep "
   alias -- find-pods-on-node='tmp_func(){ kubectl get pods --all-namespaces -o wide --field-selector spec.nodeName="$1"; unset -f tmp_func; }; tmp_func'
 fi
+
+# Use microk8s by executing `use-mk`
+use-mk() {
+  if [[ -x /snap/bin/microk8s.kubectl ]]; then
+    source <(/snap/bin/microk8s.kubectl completion zsh)
+    complete -F __start_kubectl k
+    alias kns='mkubectl config set-context --current --namespace'
+  else
+    echo "ERROR: /snap/bin/microk8s.kubectl was not found."
+  fi
+}
+
+# Execute into a pod either interactively or running a cmd
+ke() {
+  if [ $# -gt 2 ]; then
+    # Assume we aren't running interactively
+    kubectl exec "$1" -- ${@:2}
+  else
+    if [[ ! -v 2 ]]; then # If no shell was specified, we test is bash exists first.
+      kubectl exec "$1" -- [ -x /bin/bash ] &> /dev/null
+      if [ $? -eq 0 ]; then
+        2='/bin/bash'
+      else
+        2='/bin/sh'
+      fi
+    fi
+    kubectl exec -it "$1" -- ${2:-/bin/sh}
+  fi
+}
+
+# List the pods that should be targets for a service. I say should because this doesn't check readiness.
+kpods-for-service() {
+  k get pods --selector $(k get service $1 -o json | jq -r '.spec.selector | to_entries | "\(.[0].key)=\(.[0].value)"')
+}
