a02cda5ec795d5ff234ad4647dbce25c48b9568f
Zan Loy <zan.loy@gmail.com>
2020-10-28 14:59:01 -0400

    added go/bin to path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,13 @@
 # path, the 0 in the filename causes this to load first
-path+=("$HOME/bin")
+path+=("$HOME/.local/bin")
 path+=("$HOME/.dotfiles/bin")
 
+# golang
+if [ -d "$HOME/go/bin" ]; then
+  path+=("$HOME/go/bin")
+fi
+
+# snap
 if [ -d '/snap/bin' ]; then
   path+=('/snap/bin')
 fi
