fe74875daf84e43b0e3ea08de2f65edf4862a1e0
Zan Loy <zan.loy@gmail.com>
2021-03-01 20:00:11 -0500

    fix to ignore if zsh dir is missing

--- zsh/zzz-local.zsh
+++ zsh/zzz-local.zsh
@@ -1,4 +1,8 @@
 # This is supposed to load up any local configuration files.
-for x in ~/.config/dotfiles/zsh/*.zsh; do
-  source "$x"
-done
+
+if [[ -d ~/.config/dotfiles/zsh ]]; then
+  files=$(find ~/.config/dotfiles/zsh/ -name '*.zsh')
+  for x in ~/.config/dotfiles/zsh/*.zsh; do
+    source "$x"
+  done
+fi
