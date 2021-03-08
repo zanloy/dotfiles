b14b9b0314def41c35f7e7caeff34e890b266157
Zan Loy <zan.loy@gmail.com>
2021-03-08 16:39:13 -0500

    added dependency check to install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+good = true
+if [[ ! -x $(which git) ]]; then
+  good = false
+  echo "git is not installed."
+fi
+
+if [[ ! -x $(which rake) ]]; then
+  good = false
+  echo "rake is not install."
+fi
+
+if [[ ! good ]]; then
+  echo "Dependency check failed. Exiting..."
+  exit(1)
+fi
+
 if [ ! -d "$HOME/.dotfiles" ]; then
   echo "Installing .dotfiles..."
   git clone --depth=1 --recurse-submodules -j8 https://github.com/zanloy/dotfiles.git "$HOME/.dotfiles"
-  # TODO: Figure out why the submodules aren't getting pulled in the above command.
-  cd "$HOME/.dotfiles"
-  git submodule update --init --recursive -j8
   rake install
 else
-  echo ".dotfiles is already installed"
+  echo "~/.dotfiles is already installed"
 fi
