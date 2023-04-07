4dbe61d98c2b76319c8ac96136b45f2830b35d59
Zan Loy <zan.loy@gmail.com>
2023-04-07 11:27:13 -0400

    updated kdebug alias(es)

--- zsh/aws_aliases.zsh
+++ zsh/aws_aliases.zsh
@@ -1,8 +1,10 @@
+# Aliases for AWS service
+
+# ECR
 alias ecrlogin='aws ecr get-login-password --region us-gov-west-1 | docker login --username AWS --password-stdin $(aws sts get-caller-identity --query Account --output text).dkr.ecr.us-gov-west-1.amazonaws.com'
 alias ecrpush='_ecrpush(){ docker push "$(aws sts get-caller-identity --query Account --output text)".dkr.ecr.us-gov-west-1.amazonaws.com/"$1"}; _ecrpush'
 alias ecrpull='_ecrpull(){ docker pull "$(aws sts get-caller-identity --query Account --output text)".dkr.ecr.us-gov-west-1.amazonaws.com/"$1"}; _ecrpull'
 
-# AWS ECR
 function ecrpush(){
   docker push "$(aws sts get-caller-identity --query Account --output text)".dkr.ecr.us-gov-west-1.amazonaws.com/"$1"
 }
@@ -10,3 +12,8 @@ function ecrpush(){
 function ecrpull(){
   docker pull "$(aws sts get-caller-identity --query Account --output text)".dkr.ecr.us-gov-west-1.amazonaws.com/"$1"
 }
+
+# EKS
+alias eksdev='aws eks update-kubeconfig --name bip-dev-eabcf9b3'
+alias ekssbx='aws eks update-kubeconfig --name bip-sbx-841804c5'
+alias eksprd='aws eks update-kubeconfig --name bip-prod-b7dab922'
